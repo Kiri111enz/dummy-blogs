cce099a40210b9befa96fa2ae4ba959b05c8edaa
Kiri111enz <omelyanenkokirill@gmail.com>
2023-09-12 11:34:06 +0300

    blogs page header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
+import SearchBar from 'components/SearchBar';
 import { NextPage } from 'next';
+import styles from 'styles/blogs.module.css';
 
-const Home: NextPage = () => {
+const Blogs: NextPage = () => {
     return (
-        <>Home</>
+        <div className={styles.container}>
+            <header>
+                <h1 className={styles.title}>Блог</h1>
+                <p className='text-normal'>Здесь мы делимся интересными кейсами из наших проектов, пишем про IT, а также переводим зарубежные статьи</p>
+                <SearchBar placeholder='Поиск по названию статьи' />
+            </header>
+        </div>
     );
 };
 
-export default Home;
+export default Blogs;
